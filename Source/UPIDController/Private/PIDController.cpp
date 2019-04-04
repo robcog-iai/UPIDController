49f5cadb57dde03641b618c8d267e941a5dd6bc7
Andrei Haidu <andrei.haidu@yahoo.com>
2019-04-04 11:24:03 +0200

    moved the function definitions to cpp

---
+++ Source/UPIDController/Private/PIDController.cpp
@@ -0,0 +1,147 @@
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
+// Author: Andrei Haidu (http://haidu.eu)
+
+#include "PIDController.h"
+
+// Init ctor
+FPIDController::FPIDController(float InP, float InI, float InD, float InMaxOutAbs)
+	: P(InP), I(InI), D(InD), MaxOutAbs(InMaxOutAbs)
+{
+	// Reset errors, bind update function ptr
+	FPIDController::Init();
+}
+
+// Init 
+void FPIDController::Init(float InP, float InI, float InD, float InMaxOutAbs)
+{
+	P = InP;
+	I = InI;
+	D = InD;
+	MaxOutAbs = InMaxOutAbs;
+	// Reset errors, bind update function ptr
+	FPIDController::Init();
+}
+
+// Default init
+void FPIDController::Init()
+{
+	PrevErr = 0.f;
+	IErr = 0.f;
+
+	// Bind the update type function ptr
+	if (P > 0.f && I > 0.f && D > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController::UpdateAsPID;
+	}
+	else if (P > 0.f && I > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController::UpdateAsPI;
+	}
+	else if (P > 0.f && D > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController::UpdateAsPD;
+	}
+	else if (P > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController::UpdateAsP;
+	}
+	else
+	{
+		// Default
+		UpdateFunctionPtr = &FPIDController::UpdateAsPID;
+	}
+}
+
+// Call the update function pointer
+FORCEINLINE float FPIDController::Update(const float InError, const float InDeltaTime)
+{
+	return (this->*UpdateFunctionPtr)(InError, InDeltaTime);
+}
+
+
+FORCEINLINE float FPIDController::UpdateAsPID(const float InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || FMath::IsNaN(InError))
+	//{
+	//	return 0.0f;
+	//}
+
+	// Calculate proportional output
+	const float POut = P * InError;
+
+	// Calculate integral error / output
+	IErr += InDeltaTime * InError;
+	const float IOut = I * IErr;
+
+	// Calculate the derivative error / output
+	const float DErr = (InError - PrevErr) / InDeltaTime;
+	const float DOut = D * DErr;
+
+	// Set previous error
+	PrevErr = InError;
+
+	// Calculate the output
+	const float Out = POut + IOut + DOut;
+
+	// Clamp output
+	return FMath::Clamp(Out, -MaxOutAbs, MaxOutAbs);
+}
+
+FORCEINLINE float FPIDController::UpdateAsP(const float InError, const float /*InDeltaTime*/)
+{
+	//if (FMath::IsNaN(InError))
+	//{
+	//	return 0.0f;
+	//}
+
+	// Calculate proportional output
+	const float Out = P * InError;
+
+	// Clamp output
+	return FMath::Clamp(Out, -MaxOutAbs, MaxOutAbs);
+}
+
+FORCEINLINE float FPIDController::UpdateAsPD(const float InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || FMath::IsNaN(InError))
+	//{
+	//	return 0.0f;
+	//}
+
+	// Calculate proportional output
+	const float POut = P * InError;
+
+	// Calculate the derivative error / output
+	const float DErr = (InError - PrevErr) / InDeltaTime;
+	const float DOut = D * DErr;
+
+	// Set previous error
+	PrevErr = InError;
+
+	// Calculate the output
+	const float Out = POut + DOut;
+
+	// Clamp output
+	return FMath::Clamp(Out, -MaxOutAbs, MaxOutAbs);
+}
+
+FORCEINLINE float FPIDController::UpdateAsPI(const float InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || FMath::IsNaN(InError))
+	//{
+	//	return 0.0f;
+	//}
+
+	// Calculate proportional output
+	const float POut = P * InError;
+
+	// Calculate integral error / output
+	IErr += InDeltaTime * InError;
+	const float IOut = I * IErr;
+
+	// Calculate the output
+	const float Out = POut + IOut;
+
+	// Clamp output
+	return FMath::Clamp(Out, -MaxOutAbs, MaxOutAbs);
+}
---
+++ Source/UPIDController/Private/PIDController3D.cpp
@@ -0,0 +1,147 @@
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
+// Author: Andrei Haidu (http://haidu.eu)
+
+#include "PIDController3D.h"
+
+// Init ctor
+FPIDController3D::FPIDController3D(float InP, float InI, float InD, float InMaxOutAbs)
+	: P(InP), I(InI), D(InD), MaxOutAbs(InMaxOutAbs)
+{
+	// Reset errors, bind update function ptr
+	FPIDController3D::Init();
+}
+
+// Init
+void FPIDController3D::Init(float InP, float InI, float InD, float InMaxOutAbs)
+{
+	P = InP;
+	I = InI;
+	D = InD;
+	MaxOutAbs = InMaxOutAbs;
+	// Reset errors, bind update function ptr
+	FPIDController3D::Init();
+}
+
+// Default init
+void FPIDController3D::Init()
+{
+	PrevErr = FVector(0.f);
+	IErr = FVector(0.f);
+
+	// Bind the update type function ptr
+	if (P > 0.f && I > 0.f && D > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController3D::UpdateAsPID;
+	}
+	else if (P > 0.f && I > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController3D::UpdateAsPI;
+	}
+	else if (P > 0.f && D > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController3D::UpdateAsPD;
+	}
+	else if (P > 0.f)
+	{
+		UpdateFunctionPtr = &FPIDController3D::UpdateAsP;
+	}
+	else
+	{
+		// Default
+		UpdateFunctionPtr = &FPIDController3D::UpdateAsPID;
+	}
+}
+
+
+// Call the update function pointer
+FORCEINLINE FVector FPIDController3D::Update(const FVector InError, const float InDeltaTime)
+{
+	return (this->*UpdateFunctionPtr)(InError, InDeltaTime);
+}
+
+FORCEINLINE FVector FPIDController3D::UpdateAsPID(const FVector InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || InError.ContainsNaN())
+	//{
+	//	return FVector(0.0f);
+	//}
+
+	// Calculate proportional output
+	const FVector POut = P * InError;
+
+	// Calculate integral error / output
+	IErr += InDeltaTime * InError;
+	const FVector IOut = I * IErr;
+
+	// Calculate the derivative error / output
+	const FVector DErr = (InError - PrevErr) / InDeltaTime;
+	const FVector DOut = D * DErr;
+
+	// Set previous error
+	PrevErr = InError;
+
+	// Calculate the output
+	const FVector Out = POut + IOut + DOut;
+
+	// Clamp the vector values
+	return Out.BoundToCube(MaxOutAbs);
+}
+
+FORCEINLINE FVector FPIDController3D::UpdateAsP(const FVector InError, const float /*InDeltaTime*/)
+{
+	//if (InError.ContainsNaN())
+	//{
+	//	return FVector(0.0f);
+	//}
+
+	// Calculate proportional output
+	const FVector Out = P * InError;
+
+	// Clamp output
+	return Out.BoundToCube(MaxOutAbs);
+}
+
+FORCEINLINE FVector FPIDController3D::UpdateAsPD(const FVector InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || InError.ContainsNaN())
+	//{
+	//	return FVector(0.0f);
+	//}
+
+	// Calculate proportional output
+	const FVector POut = P * InError;
+
+	// Calculate the derivative error / output
+	const FVector DErr = (InError - PrevErr) / InDeltaTime;
+	const FVector DOut = D * DErr;
+
+	// Set previous error
+	PrevErr = InError;
+
+	// Calculate the output
+	const FVector Out = POut + DOut;
+
+	// Clamp output
+	return Out.BoundToCube(MaxOutAbs);
+}
+
+FORCEINLINE FVector FPIDController3D::UpdateAsPI(const FVector InError, const float InDeltaTime)
+{
+	//if (InDeltaTime == 0.0f || InError.ContainsNaN())
+	//{
+	//	return FVector(0.0f);
+	//}
+
+	// Calculate proportional output
+	const FVector POut = P * InError;
+
+	// Calculate integral error / output
+	IErr += InDeltaTime * InError;
+	const FVector IOut = I * IErr;
+
+	// Calculate the output
+	const FVector Out = POut + IOut;
+
+	// Clamp output
+	return Out.BoundToCube(MaxOutAbs);
+}
